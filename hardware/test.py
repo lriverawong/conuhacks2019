cffb0650b98c6481fc9d9cb636e2fcb82b2c5d8e
Niroigen Suntharam <niroigen@gmail.com>
2019-01-26 15:51:05 -0500

    Adding command line arguments to move the servo

--- hardware/test.py
+++ hardware/test.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 
+import sys
+
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(4, GPIO.OUT)
@@ -8,14 +10,12 @@ GPIO.setup(4, GPIO.OUT)
 p = GPIO.PWM(4, 50)
 p.start(2)
 
-try:
-	while True:
-		p.ChangeDutyCycle(7.5)
-		time.sleep(1)
-		p.ChangeDutyCycle(12.5)
-		time.sleep(1)
-		p.ChangeDutyCycle(2.5)
-		time.sleep(1)
+p.ChangeDutyCycle(sys.argv[1])
+		# time.sleep(1)
+		# p.ChangeDutyCycle(12.5)
+		# time.sleep(1)
+		# p.ChangeDutyCycle(2.5)
+		# time.sleep(1)
 
 except KeyboardInterrupt:
 	GPIO.cleanup()

e7e11cde013abb4a57f5da71837550fc55eea1fe
Niroigen Suntharam <niroigen@gmail.com>
2019-01-26 16:07:10 -0500

    Adding command line arguments

--- hardware/test.py
+++ hardware/test.py
@@ -2,7 +2,6 @@ import RPi.GPIO as GPIO
 import time
 
 import sys
-
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(4, GPIO.OUT)
@@ -10,11 +9,16 @@ GPIO.setup(4, GPIO.OUT)
 p = GPIO.PWM(4, 50)
 p.start(2)
 
-p.ChangeDutyCycle(float(sys.argv[1]))
+try:
+	while True:
+		mode=float(raw_input('Input:'))
+		p.ChangeDutyCycle(mode)
 		# time.sleep(1)
 		# p.ChangeDutyCycle(12.5)
 		# time.sleep(1)
 		# p.ChangeDutyCycle(2.5)
 		# time.sleep(1)
 
-GPIO.cleanup()
+except KeyboardInterrupt:
+	GPIO.cleanup()
+	print "Not a number"

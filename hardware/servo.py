6f202d8b89d33753f4bc772f29e1b241d57fd3c0
Niroigen Suntharam <niroigen@gmail.com>
2019-01-26 22:11:12 -0500

    Resetting back to default location

--- hardware/servo.py
+++ hardware/servo.py
@@ -2,18 +2,26 @@ import RPi.GPIO as GPIO
 import time
 
 import sys
-GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(4, GPIO.OUT)
+def init():
+	GPIO.setmode(GPIO.BCM)
 
-p = GPIO.PWM(4, 50)
-p.start(2)
+	GPIO.setup(4, GPIO.OUT)
+
+	p = GPIO.PWM(4, 50)
+	p.start(2)
+
+def cleanup():
+	GPIO.cleanup()
 
 try:
 	while True:
 		mode=float(raw_input('Input:'))
+		init()
 		p.ChangeDutyCycle(mode)
+		time.sleep(1)
+		cleanup()
 
 except KeyboardInterrupt:
-	GPIO.cleanup()
+	cleanup()
 	print "Not a number"

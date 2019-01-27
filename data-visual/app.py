8a553bb90565a7d5c780001214509f0b62bdd91a
Luis Rivera-Wong <lriverawong@outlook.com>
2019-01-27 01:56:52 -0500

    mod

--- data-visual/app.py
+++ data-visual/app.py
@@ -18,8 +18,8 @@ app.layout = html.Div(children=[
         id='example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                {'x': [1, 2], 'y': [4, 1]},
+                {'x': [1, 2], 'y': [2, 4]},
             ],
             'layout': {
                 'title': 'Dash Data Visualization'

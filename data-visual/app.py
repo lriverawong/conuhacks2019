8e6f26af1547df7affe44cf6196e186be02ed7bd
Luis Rivera-Wong <lriverawong@outlook.com>
2019-01-27 03:20:55 -0500

    new vis

--- data-visual/app.py
+++ data-visual/app.py
@@ -2,6 +2,8 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -16,16 +18,17 @@ app.layout = html.Div(children=[
 
     dcc.Graph(
         id='example-graph',
-        figure={
-            labels = ['Blue Bin','Grey Bin','Green Bin','Garbare'],
-            values = [4500,2500,1053,500],
-            colors = ['#375190', '#737680', '#4B8B4F', '#8B6D4B'],
-            trace = go.Pie(labels=labels, values=values,
-                    hoverinfo='label+percent', textinfo='value + label',
-                    textfont=dict(size=30),
-                    marker=dict(colors=colors,
-                                line=dict(color='#000000', width=1)))
-        }
+        labels = ['Blue Bin','Grey Bin','Green Bin','Garbare'],
+        values = [4500,2500,1053,500],
+        colors = ['#375190', '#737680', '#4B8B4F', '#8B6D4B'],
+        trace = go.Pie(labels=labels, values=values,
+                hoverinfo='label+percent', textinfo='value + label',
+                textfont=dict(size=30),
+                marker=dict(colors=colors,
+                            line=dict(color='#000000', width=1)))
+        # figure={
+            
+        # }
     )
 ])
 

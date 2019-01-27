aea901400ad48c73083535800b1e93bf411066bc
Niroigen Suntharam <niroigen@gmail.com>
2019-01-26 22:39:13 -0500

    Attempting to fix jitter part 2

--- hardware/servo.py
+++ hardware/servo.py
@@ -7,12 +7,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.OUT)
 
 p = GPIO.PWM(4, 50)
-p.start(2)
 
 try:
 	while True:
 		mode=float(raw_input('Input:'))
-		p.start()
+		p.start(2)
 		p.ChangeDutyCycle(mode)
 		p.stop()
 

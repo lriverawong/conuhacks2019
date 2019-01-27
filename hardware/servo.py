61af456fb6f6b6c6692e887f40c7659f250da110
Niroigen Suntharam <niroigen@gmail.com>
2019-01-26 22:56:58 -0500

    Reverting

--- hardware/servo.py
+++ hardware/servo.py
@@ -2,20 +2,17 @@ import RPi.GPIO as GPIO
 import time
 
 import sys
+GPIO.setmode(GPIO.BCM)
 
-def move_servo(mode):
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(4, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
 
-	p = GPIO.PWM(4, 50)
-	p.start(2)
-	p.ChangeDutyCycle(mode)
-	GPIO.cleanup()
+p = GPIO.PWM(4, 50)
+p.start(2)
 
 try:
 	while True:
 		mode=float(raw_input('Input:'))
-		move_servo(mode)
+		p.ChangeDutyCycle(mode)
 
 except KeyboardInterrupt:
 	GPIO.cleanup()

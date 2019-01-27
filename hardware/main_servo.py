6cd331d244fe3701dec209a27e1cfe42aab8d98d
Hacker <hacker@pi.com>
2019-01-27 07:22:10 +0000

    port 80 config?

--- hardware/main_servo.py
+++ hardware/main_servo.py
@@ -1,5 +1,22 @@
 from servo import move_servo
 
 def classify_trash(class_type):
-	move_servo(4,2, delay=1)
-	move_servo(17,3)
+	if class_type == 0:
+		move_servo(4,9, delay=1)
+		move_servo(17,9.5)
+		move_servo(4,8)
+		move_servo(17,7.5)
+
+	elif class_type == 1:
+		move_servo(4,5, delay=1)
+		move_servo(17,11.5)
+		move_servo(17,8)
+	elif class_type == 2:
+		move_servo(4,6, delay=1)
+		move_servo(17,12)
+		move_servo(17,8)
+	else:
+		move_servo(17,3.5)
+		move_servo(17,8)
+
+classify_trash(3)

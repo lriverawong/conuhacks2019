9770c86749f83bdb411e3cb5f36825706d2febf5
Niroigen Suntharam <niroigen@gmail.com>
2019-01-26 22:21:57 -0500

    Making servo clas

--- hardware/servo.py
+++ hardware/servo.py
@@ -2,18 +2,35 @@ import RPi.GPIO as GPIO
 import time
 
 import sys
-GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(4, GPIO.OUT)
+class Servo:
+	def __init__():
+		init_GPIO()
 
-p = GPIO.PWM(4, 50)
-p.start(2)
+	def init_GPIO():
+		GPIO.setmode(GPIO.BCM)
+
+		GPIO.setup(4, GPIO.OUT)
+
+		self.p = GPIO.PWM(4, 50)
+		self.p.start(2)
+
+	def cleanup():
+		GPIO.cleanup()
+
+	def change_duty_cycle(mode):
+		self.p.ChangeDutyCycle(mode)
+
+s = Servo()
 
 try:
 	while True:
 		mode=float(raw_input('Input:'))
-		p.ChangeDutyCycle(mode)
+		s.init_GPIO()
+		s.change_duty_cycle(mode)
+		time.sleep(1)
+		s.cleanup()
 
 except KeyboardInterrupt:
-	GPIO.cleanup()
+	cleanup()
 	print "Not a number"

d42390bcb4a2486a2117d29f07fd312e524bf74e
Hacker <hacker@pi.com>
2019-01-27 06:16:03 +0000

    fixed file path changes

--- ai-api/runner.sh
+++ ai-api/runner.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export GOOGLE_APPLICATION_CREDENTIALS=/home/hacker/conuhacks2019-caa53f7c0606.json
-python ~/conuhacks2019/ai-api/object_detection.py
+python /home/hacker/conuhacks2019/ai-api/object_detection.py
 
 echo "---done script---"
--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-inotifywait -m ~/image-bank -e create -e moved_to |
+inotifywait -m /home/hacker/image-bank -e create -e moved_to |
     while read path action file; do
         echo "The file '$file' appeared in directory '$path' via '$action'"
-        bash ~/conuhacks2019/ai-api/runner.sh
+        bash /home/hacker/conuhacks2019/ai-api/runner.sh
     done
